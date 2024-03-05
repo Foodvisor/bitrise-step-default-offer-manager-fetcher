8bd45bcf91aa7e90c9105f0ed7c241fdd3455539
Louise Foodvisor <louise@foodvisor.io>
2024-03-05 11:41:53 +0100

    Fix exec command line in step.sh

--- step.sh
+++ step.sh
@@ -3,4 +3,4 @@
 set -ex
 git clone git@github.com:Foodvisor/default-offer-manager-fetcher.git
 xcodebuild -project default-offer-manager-fetcher/default-offer-manager-fetcher/default-offer-manager-fetcher.xcodeproj -configuration Release
-default-offer-manager-fetcher/build/Release/default-offer-manager-fetcher -env "${env}" -version "${version}"
+default-offer-manager-fetcher/default-offer-manager-fetcher/build/Release/default-offer-manager-fetcher -env "${env}" -version "${version}"

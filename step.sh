a7d3b46d3d2cb913cbc527631ca7741e64859522
Louise Foodvisor <louise@foodvisor.io>
2024-03-05 14:48:55 +0100

    Add default offer json to artifact

--- step.sh
+++ step.sh
@@ -4,3 +4,4 @@ set -ex
 git clone git@github.com:Foodvisor/default-offer-manager-fetcher.git
 xcodebuild -project default-offer-manager-fetcher/default-offer-manager-fetcher/default-offer-manager-fetcher.xcodeproj -configuration Release
 default-offer-manager-fetcher/default-offer-manager-fetcher/build/Release/default-offer-manager-fetcher -env "${env}" -version "${version}"
+cp ./Foodvisor/App/Premium/Offers/default/default-offers.json $BITRISE_DEPLOY_DIR
